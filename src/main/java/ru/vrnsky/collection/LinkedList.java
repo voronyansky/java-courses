7daf7892706d444513752438c414f8eec9773813
Egor Voronyansky <voronyansky.egor@yandex.ru>
2016-04-14 23:51:15 +0400

    Add my implementation of linked list
    Add my implementation of linked list

--- src/main/java/ru/vrnsky/collection/LinkedList.java
+++ src/main/java/ru/vrnsky/collection/LinkedList.java
@@ -1,27 +1,68 @@
 package ru.vrnsky.collection;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 /**
- * Created by Egor on 30.03.2016.
+ * Implementation of linked list
+ * @see Collection
+ * @see java.util.LinkedList
  */
 public class LinkedList<T> implements List<T> {
 
+    Element<T> start; //Start list
+    Element<T> finish; //Finish list
+
+    /**
+     * Default constructor create empty list
+     */
+    public LinkedList()
+    {
+        start = new Element<T>(null,null,0);
+        finish = new Element<T>((T)start, null,1);
+        start.next = finish.prev;
+    }
+
+    /**
+     * Compute size of list by cross through all elements
+     * @return count of elements in list
+     */
     @Override
     public int size() {
-        return 0;
+
+        int countElements = 0;
+        for(Element<T> current = start; current.next != null;)
+        {
+            countElements++;
+            current = (Element<T>)current.next;
+        }
+
+        return countElements;
     }
 
+    /**
+     * Check if array empty or non empty
+     * @return true if list is empty and false if list is empty
+     */
     @Override
     public boolean isEmpty() {
-        return false;
+
+        boolean isEmpty = start.next == finish.prev;
+        return isEmpty;
     }
 
+    /**
+     * Fail on test. Doesn't work
+     * @param o - object for find in list
+     * @return - true if list have this object and false otherwise
+     */
     @Override
     public boolean contains(Object o) {
+
+        boolean contains = false;
+        for(Element<T> current = start; current.next != null;)
+        {
+            if(current.equals(o)) return true;
+        }
         return false;
     }
 
@@ -30,9 +71,20 @@ public class LinkedList<T> implements List<T> {
         return null;
     }
 
+    /**
+     * All elements in array view
+     * @return Object[] which contains all elements of list
+     */
     @Override
     public Object[] toArray() {
-        return new Object[0];
+
+        Object[] array = new Object[size()];
+        for(int index = 0; index < array.length; index++)
+        {
+            array[index] = get(index);
+        }
+
+        return array;
     }
 
     @Override
@@ -40,9 +92,16 @@ public class LinkedList<T> implements List<T> {
         return null;
     }
 
+    /**
+     * Push element to the end of list
+     * @param t - element for adding
+     * @return true if ok otherwise false
+     */
     @Override
     public boolean add(T t) {
-        return false;
+
+        boolean result = false;
+        return result;
     }
 
     @Override
@@ -80,48 +139,87 @@ public class LinkedList<T> implements List<T> {
 
     }
 
+    /**
+     * Return element which placed at the index position
+     * @param index
+     * @return
+     */
     @Override
     public T get(int index) {
+
+        if(index >= 0 && index <= size()) {
+            for (Element<T> current = start; current != null; ) {
+                if (current.index == index)
+                    return (T) current;
+            }
+        }
+
         return null;
     }
 
+    //TODO write docs and implementation
     @Override
     public T set(int index, T element) {
         return null;
     }
 
+    //TODO write docs and implementation
     @Override
     public void add(int index, T element) {
 
     }
 
+    //TODO write docs and implementation
     @Override
     public T remove(int index) {
-        return null;
+
+       return null;
     }
 
+    //TODO write docs and implementation
     @Override
     public int indexOf(Object o) {
         return 0;
     }
 
+    //TODO write docs
     @Override
     public int lastIndexOf(Object o) {
         return 0;
     }
 
+    //TODO write docs
     @Override
     public ListIterator<T> listIterator() {
         return null;
     }
 
+    //TODO write docs
     @Override
     public ListIterator<T> listIterator(int index) {
         return null;
     }
 
+    //TODO write docs
     @Override
     public List<T> subList(int fromIndex, int toIndex) {
         return null;
+    }
+
+
+    //TODO write docs
+    protected class Element<T>
+    {
+        T prev;
+        T next;
+        int index;
+
+        public Element(T prev, T next, int index)
+        {
+            this.prev = prev;
+            this.next = next;
+            this.index = index;
+        }
+
     }
 }
